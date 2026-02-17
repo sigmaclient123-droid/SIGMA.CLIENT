d9189b466010df358339598ceb2dc6a30b224b1e
sigmaclient123-droid <sigmaclient123@gmail.com>
2026-02-17 13:11:15 -0600

    Add files via upload

---
+++ PluginInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace StupidTemplate
+{
+    public class PluginInfo
+    {
+        public const string GUID = "sigmaboyjdshfjksdgfjhgfjhdsgfjhdsgfjhs.com";
+        public const string Name = "sigma.client";
+        public const string Description = "sigmas on top creds to dot and cdev";
+        public const string Version = "1.9.6";
+    }
+}
